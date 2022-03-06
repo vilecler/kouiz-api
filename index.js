eb09aa9976aad85206d5af9a7ae4b3fe71faaaf5
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-03-06 22:06:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
-  let responseMessage = 'Hello, World! I am Vivien ;).';
+  let responseMessage = 'Hello, World! I am Vivien  ffgdfedfdfdf ;).';
 
   return {
     statusCode: 200,
