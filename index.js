9e985cf72347e6e1ccc81d97f38b958ddd373859
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-04-25 00:30:33 +0200

    Testing MongoDb atlas connection

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const AWS = require("aws-sdk");
-const dynamo = new AWS.DynamoDB.DocumentClient();
+
+/* Configuring mongodb connection only once */
+const { MongoClient } = require('mongodb');
+
+const MONGODB_CONNECTION_STRING = new URL(process.env.MONGODB_URI);
+MONGODB_CONNECTION_STRING.username = process.env.AWS_ACCESS_KEY_ID;
+MONGODB_CONNECTION_STRING.password = encodeURIComponent(process.env.AWS_SECRET_ACCESS_KEY);
+MONGODB_CONNECTION_STRING.searchParams.set('authSource', '$external');
+MONGODB_CONNECTION_STRING.searchParams.set('authMechanism', 'AWS_SESSION_TOKEN:' + encodeURIComponent(process.env.AWS_SESSION_TOKEN));
+
+let client = new MongoClient(MONGODB_CONNECTION_STRING.toString(), { useNewUrlParser: true, useUnifiedTopology: true });
+const clientPromise = client.connect();
+/* End configuring mongodb */
 
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
+<<<<<<< Updated upstream
   let responseMessage = 'Hello, World! I am Vivien ;).';
+=======
+  const client = await clientPromise;
+  let responseMessage = 'Hello, World! I am Vivien ' + client.db().databaseName + ' ;).';
+>>>>>>> Stashed changes
 
   return {
     statusCode: 200,
