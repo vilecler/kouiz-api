19692ccc5952f6af4f14112f9be195cbd6dcdad6
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-04-25 20:37:29 +0200

    WIP

--- config/constants.js
+++ config/constants.js
@@ -3,8 +3,6 @@ const MONGODB_URI  = process.env.MONGODB_URI;
 const MONGODB_USER = process.env.MONGODB_USER;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 const MONGODB_DATABASE = process.env.MONGODB_DATABASE;
-
-console.log(process.env);
 const MONGODB_CONNECTION_STRING = `${MONGODB_URI.split('://')[0]}://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URI.split('://')[1]}/${MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const AWS = require("aws-sdk");
 //const env = require("./config/env");
 const dbo = require("./config/db");
 
+const { MONGODB_CONNECTION_STRING, MONGODB_DATABASE } = require('./config/constants');
+
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
 
   const client = await clientPromise;
-  let responseMessage = 'Testing Database connection :) ' + dbo.getDatabaseConnection().databaseName;
+  let responseMessage = 'Testing Database connection :) ' + MONGODB_CONNECTION_STRING;
 
   return {
     statusCode: 200,
