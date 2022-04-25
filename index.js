7af8bf251e688db7e5eef9f4f579fe81ceb200fd
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-04-25 02:06:18 +0200

    Connection to MongoDB atlas done

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const AWS = require("aws-sdk");
 /* Configuring mongodb connection only once */
 const { MongoClient } = require('mongodb');
 
-const MONGODB_CONNECTION_STRING = new URL(process.env.MONGODB_URI);
-MONGODB_CONNECTION_STRING.username = process.env.AWS_ACCESS_KEY_ID;
-MONGODB_CONNECTION_STRING.password = encodeURIComponent(process.env.AWS_SECRET_ACCESS_KEY);
-MONGODB_CONNECTION_STRING.searchParams.set('authSource', '$external');
-MONGODB_CONNECTION_STRING.searchParams.set('authMechanism', 'AWS_SESSION_TOKEN:' + encodeURIComponent(process.env.AWS_SESSION_TOKEN));
+const uri = `${process.env.MONGODB_URI.split('://')[0]}://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI.split('://')[1]}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
 
-//let client = new MongoClient(MONGODB_CONNECTION_STRING.toString(), { useNewUrlParser: true, useUnifiedTopology: true });
-//const clientPromise = client.connect();
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const clientPromise = client.connect();
 /* End configuring mongodb */
 
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
 
-  //const client = await clientPromise;
-  let responseMessage = MONGODB_CONNECTION_STRING.toString() + '\n' + process.env.MONGODB_URI + '\n' + process.env.AWS_ACCESS_KEY_ID + '\n' + process.env.AWS_SECRET_ACCESS_KEY + '\n' + process.env.AWS_SESSION_TOKEN;
+  const client = await clientPromise;
+  let responseMessage = 'Bite :) ' + client.db().databaseName;
+
 
   return {
     statusCode: 200,
