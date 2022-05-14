d0b69c99dd0c99ae9b71dc093288faf6abfee9f8
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-05-14 20:55:43 +0200

    Creating getCategories

--- src/handlers/category.ts
+++ src/handlers/category.ts
@@ -18,3 +18,13 @@ export const getCategoryHandler = async (parameters: any, event: APIGatewayEvent
     })
   };
 };
+
+
+export const getCategoriesHandler = async (parameters: any, event: APIGatewayEvent, context: Context, database: Db): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'These are categories'
+    })
+  };
+};
--- src/routing/category.ts
+++ src/routing/category.ts
@@ -1,8 +1,10 @@
 import { HTTPMethods, RoutingItem } from './routingitem';
 var UrlPattern = require('url-pattern');
 
-import { getCategoryHandler } from '../handlers/category';
+import { getCategoriesHandler, getCategoryHandler } from '../handlers/category';
 
 export const categoryRoutes = [
   new RoutingItem(HTTPMethods.Get, new UrlPattern('/categories/:id'), getCategoryHandler),
+  new RoutingItem(HTTPMethods.Get, new UrlPattern('/categories'), getCategoriesHandler),
 ];
+q
