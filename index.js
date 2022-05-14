d65edcb085d60fffc8d18889f8b9510c67465708
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-05-14 14:16:00 +0200

    Set default handler

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*const AWS = require("aws-sdk");
-//const env = require("./config/env");
-const clientPromise = require("./config/db");
+const handler = require("./dist/handlers/index");
 
-module.exports.handler = async (event) => {
-  console.log('Event: ', event);
-
-  client = await clientPromise;
-  let responseMessage = 'Testing Database connection :) ' + client.db().databaseName;
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: responseMessage,
-    }),
-  }
-}*/
+module.exports.handler = handler;
