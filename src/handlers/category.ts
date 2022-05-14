3ba8ed8f0c6ddb7ff9cfaedc55156c8e1a9957ca
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-05-14 21:25:43 +0200

    Creating getter for category

--- src/handlers/category.ts
+++ src/handlers/category.ts
@@ -1,30 +1,86 @@
 import { APIGatewayProxyResult, APIGatewayEvent, Context } from 'aws-lambda';
 import { Db } from 'mongodb';
 
+import { Category } from "../models/category";
+
+const CATEGORY_COLLECTION = "category";
+
 export const getCategoryHandler = async (parameters: any, event: APIGatewayEvent, context: Context, database: Db): Promise<APIGatewayProxyResult> => {
-  if(!parameters.id){
+  if(!parameters.code){
     return {
       statusCode: 400,
       body: JSON.stringify({
-          message: 'Id is missing.'
+          message: 'Code is missing.'
       })
     };
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'This is a category. ' + parameters.id
-    })
-  };
+  try {
+    const query = { code: parameters.code };
+    const category = (await database.collection(CATEGORY_COLLECTION).findOne(query)) as Category;
+
+    if (!category){
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+            message: 'Cateogory not found.'
+        })
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+          category: category
+      })
+    };
+
+  } catch(error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+          message: 'Unable to find any category. Id used is: ' + parameters.id
+      })
+    };
+  }
+
 };
 
 
 export const getCategoriesHandler = async (parameters: any, event: APIGatewayEvent, context: Context, database: Db): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'These are categories'
-    })
-  };
+  //Retrieve falcutative parameter
+  let isHidden: boolean = false;
+  if (event!.queryStringParameters!.isHidden){
+    isHidden = Boolean(event!.queryStringParameters!.isHidden);
+  }
+
+  try{
+    const query = (isHidden) ? {} : {isHidden: false};  //By default hidden categories are not visible unless isHidden falcutative parameter is used.
+    const categories = (await database.collection(CATEGORY_COLLECTION).find(query).toArray()) as Category[];
+
+    if (!categories){
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+            message: 'Cateogories not found.'
+        })
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+          categories: categories
+      })
+    };
+
+  } catch(error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+          message: 'Unable to find categories.'
+      })
+    };
+  }
+
 };
--- src/routing/category.ts
+++ src/routing/category.ts
@@ -4,6 +4,6 @@ var UrlPattern = require('url-pattern');
 import { getCategoriesHandler, getCategoryHandler } from '../handlers/category';
 
 export const categoryRoutes = [
-  new RoutingItem(HTTPMethods.Get, new UrlPattern('/categories/:id'), getCategoryHandler),
+  new RoutingItem(HTTPMethods.Get, new UrlPattern('/categories/:code'), getCategoryHandler),
   new RoutingItem(HTTPMethods.Get, new UrlPattern('/categories'), getCategoriesHandler),
 ];
