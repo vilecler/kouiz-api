f2e2200f486d192be23f638e4e1ee7cd091a5693
Vivien Leclercq <vivien.leclercq@gmail.com>
2022-06-28 23:00:51 +0200

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,8 +4,10 @@ import { Db, Timestamp } from 'mongodb';
 import { connectToDatabase } from "../services/db";
 import { resolveRoute } from "../routing/router";
 
-const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+const handler = async (event: any, context: Context): Promise<APIGatewayProxyResult> => {
     const database: Db = await connectToDatabase();
+
+    print(event);
 
     return await resolveRoute(event, context, database);
 };
